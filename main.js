18017e3bd40c41693dab2da23f915b926cc5e9fd
roba <10408936+roba-adnew@users.noreply.github.com>
2023-09-22 13:27:40 -0400

    Updated calculator with input field
    Update the calculator with an input field that now displays the various
    button values in the display.
    
     Changes to be committed:
    	modified:   index.html
    	modified:   main.js
    	modified:   style.css

--- main.js
+++ main.js
@@ -7,4 +7,22 @@ function operator(a,op,b) {
     }
 }
 
-console.log(operator(20,"/",10))
+function displayOnPress() {
+    // Grab the HTML elements for the display and the button
+    const buttons = document.querySelectorAll("button");
+    const display = document.getElementById('input');
+
+    // Run a listener for button presses and update the display
+    buttons.forEach(button => 
+        button.addEventListener("click", function(event) {
+            // Clear the input whenever a button is pressed
+            console.log(display);
+            display.innerHTML = "   ";
+
+            // Update the display with the given button
+            display.value = this.id;
+        })
+    );
+}
+
+displayOnPress();   
