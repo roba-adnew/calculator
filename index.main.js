1c95951e28b322bc100c84ed08e9a585ad95fb87
roba <10408936+roba-adnew@users.noreply.github.com>
2023-09-22 10:56:44 -0400

    Created starting html, js, and css files
    Created html file to contain the calculator buttons. js file contains
    the simple operator function that can add, subtract, multiply, and
    divide. Lastly, the css file styles the page to fit the calculator.
    
     Changes to be committed:
    	new file:   index.html
    	new file:   index.main.js
    	new file:   style.css

---
+++ index.main.js
@@ -0,0 +1,12 @@
+function operator(a,op,b) {
+    switch (op) {
+        case "+": return a + b;
+        case "-": return a - b;
+        case "*": return a * b;
+        case "/": return a / b;
+    }
+}
+
+
+
+console.log(operator(20,"/",10))
