8f270226e3edab4a464759cbebef8627a9d44ced
roba <10408936+roba-adnew@users.noreply.github.com>
2023-09-25 18:52:14 -0400

    Made changes to the logic of the operator
    Operator was changed to handle more secure logic
    
     Changes to be committed:
    	modified:   index.html
    	modified:   main.js

--- main.js
+++ main.js
@@ -8,21 +8,30 @@ function operator(buttonPresses) {
     let isLastPressNumber = true;
 
     const operators = {
-        "add": function(a,b) {return a + b},
-        "subtract": function(a,b) {return a - b},
-        "multiply": function(a,b) {return a * b},
-        "divide": function(a,b) {return a / b}
+        "+": function(a,b) {return a + b},
+        "-": function(a,b) {return a - b},
+        "x": function(a,b) {return a * b},
+        "/": function(a,b) {return a / b}
     };
 
     let currentNumber = "";
+    let newNumber = "";
     let currentOperator = "";
     let numOfPresses = buttonPresses.length;
 
     for (let i = 0; i <  numOfPresses; i++) {
         if (isLastPressNumber) {
-        
-            if (isNumber( buttonPresses[i] )) {
+            
+            if (isNumber(buttonPresses[i]) && i == 0) {
                 currentNumber += buttonPresses[i];
+            }
+
+            else if ((isNumber(buttonPresses[i]) && i > 0) ) {
+                currentNumber += buttonPresses[i];
+                buttonPresses[ i - 1 ] = currentNumber;
+                buttonPresses.splice(i,1);
+                numOfPresses = buttonPresses.length;
+                i -= 1;
             }
 
             else if (buttonPresses[i] in operators) {
@@ -40,18 +49,22 @@ function operator(buttonPresses) {
             else if (isNumber( buttonPresses[ i ] )) {
 
                 if ( i + 1 == numOfPresses ) { 
+                    newNumber += buttonPresses[ i ];
+                    buttonPresses[ i - 1 ] = newNumber;
+                    buttonPresses.splice(i,1);
+                    i -= 1;
                     return operators[buttonPresses[ i - 1 ]]
-                        (parseInt(currentNumber), parseInt(buttonPresses[ i ]))
+                        (parseInt(currentNumber), parseInt(newNumber))
                 }
 
                 else {
                     if (isNumber (buttonPresses[ i + 1 ])) {
-                        currentNumber += buttonPresses[ i ];
+                        newNumber += buttonPresses[ i ];
                     }
-                    else if (buttonPresses[ i + 1] in operators) {
+                    else if (buttonPresses[ i + 1 ] in operators) {
                         currentNumber = operators[buttonPresses[ i - 1 ]]
                             (parseInt(currentNumber), 
-                            parseInt(buttonPresses[ i ]))
+                            parseInt(newNumber))
                         isLastPressNumber = true;
                     }
                 }
@@ -66,6 +79,8 @@ function displayOnPress() {
     // Grab the HTML elements for the display and the button
     const buttons = document.querySelectorAll("button");
     const display = document.getElementById('input');
+    const invisibleButtons = ["add", "subtract", "multiply", "divide", 
+        "clear", "equals"]
 
     let pressRecord = [];
 
@@ -76,14 +91,19 @@ function displayOnPress() {
             display.innerHTML = "";
 
             // Update the display with the given button
-            display.value = this.id;
+
+            if (!invisibleButtons.includes(this.id)) {
+                display.value = this.innerHTML;
+            }
 
             // We need to track all of the button presses and when the user
             // hits the equal sign then we go ahead and run the operate function
 
             if (this.id == "clear") {
                 pressRecord = [];
+                display.value = " ";
             } 
+
             else {
                 if (this.id != "equals") {
                     pressRecord.push(this.id);
